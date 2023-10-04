51daf89b730c3300ee13a083a48c0c0c2b7e290b
Hidd3n-47 <122316898+Hidd3n-47@users.noreply.github.com>
2023-10-04 19:36:42 +0100

    Initial commit

---
+++ Assigmnment1/List.h
@@ -0,0 +1,166 @@
+#pragma once
+#include <list>
+
+
+template<typename T>
+struct _List_Node
+{
+	_List_Node(T value) : _Value(value) { /* Empty. */ }
+
+	T _Value = nullptr;
+	_List_Node<T>* _pNext = nullptr;
+};
+
+template<typename T>
+class _iterator
+{
+public:
+	T value() const { return _pNode->_Value; }
+
+	inline _iterator<T>& operator++(int)
+	{
+		// TODO: Add assert here if there is no next value.
+
+		this->_index++;
+		this->_pNode = _pNode->_pNext;
+
+		return *this;
+	}
+
+	inline _iterator<T>& operator++()
+	{
+		// TODO: Add assert here if there is no next value.
+
+		this->_index++;
+		this->_pNode = _pNode->_pNext;
+
+		return *this;
+	}
+
+	inline bool operator== (const _iterator<T>& _rhs) const 
+	{
+		return this->_pNode == _rhs._pNode;
+	}
+
+	inline bool operator!= (const _iterator<T>& _rhs) const 
+	{
+		return this->_pNode != _rhs._pNode;
+	}
+
+	_iterator(_List_Node<T>* _node, uint64_t _index) : _pNode(_node), _index(_index) { /* Empty. */ /*_pNode = _node; this->_index = _index - 1;*/ }
+private:
+	_List_Node<T>* _pNode;
+	uint64_t _index;
+};
+
+template<typename T>
+class List
+{
+public:
+	List<T>() { /* Empty. */ }
+	~List<T>() { /* TODO: Clear memory here! */ }
+
+	// Iterator first.
+	// Iterator end.
+
+	/// <<< ----- Inserting into the List.
+	// TODO: Look at adding a list/array at once, eg. push_back({ 1, 2});
+	inline void push_back(T _Value)
+	{
+		++_MySize;
+
+		_List_Node<T>* node = _pFirst;
+		_List_Node<T>* _new = new _List_Node<T>(_Value);
+
+		if (!_pFirst) { _pLast = _pFirst = _new; return; }
+
+		while (node->_pNext)
+		{
+			node = node->_pNext;
+		}
+
+		node->_pNext = _new;
+		_pLast = _new;
+	}
+
+	inline void push_front(T _Value)
+	{
+		++_MySize;
+
+		_List_Node<T>* _new = new _List_Node<T>(_Value);
+		_new->_pNext = _pFirst;
+		_pFirst = _new;
+
+		if (_MySize == 1) { _pLast = _new; }
+	}
+
+	//void insert(_iterator _Where, T* _Value); // TODO: Implement iterator.
+	/// ================================
+
+	/// <<< ----- Removing from the List.
+	inline void pop_back()
+	{
+		if (_MySize == 0) { return; } // TODO: Assert here due to the list being already empty.
+
+		--_MySize;
+
+		if (!_pFirst->_pNext)
+		{
+			delete _pFirst;
+			_pLast = _pFirst = nullptr;
+			return;
+		}
+
+		if (!_pFirst->_pNext->_pNext)
+		{
+			delete _pFirst->_pNext;
+			_pLast = _pFirst;
+			return;
+		}
+
+		_pLast = _pFirst;
+
+		while (_pLast->_pNext->_pNext)
+		{
+			_pLast = _pLast->_pNext;
+		}
+
+		delete _pLast->_pNext;
+		_pLast->_pNext = nullptr;
+	}
+
+	inline void pop_front()
+	{
+		if (_MySize == 0) { return; } // TODO: Assert here due to the list being already empty.
+
+		--_MySize;
+
+		_List_Node<T> _tmp = _pFirst->_pNext;
+		delete _pFirst;
+		_pFirst = _tmp;
+	}
+
+	inline void clear()
+	{
+		// TODO: implement the clear method.
+	}
+	/// ================================
+
+	///<<< ----- Iterators.
+	inline _iterator<T> begin() { /*return _pFirst;*/ return _iterator<T>(_pFirst, 0); }
+
+	inline _iterator<T> end() { /*return _pLast;*/ return _iterator<T>(_pLast, _MySize - 1); }
+	/// ================================
+
+
+	///<<< ----- Accessors.
+	inline T front() const { return _pFirst->_Value; }
+
+	inline T back() const { return _pLast->_Value; }
+	/// ================================
+private:
+	_List_Node<T>* _pFirst = nullptr;
+	_List_Node<T>* _pLast = nullptr;
+	uint64_t _MySize = 0;
+};
+
---
+++ Assigmnment1/main.cpp
@@ -0,0 +1,232 @@
+#include <iostream>
+
+#include "List.h"
+
+#include <list>
+
+void TestOne()
+{
+	List<int> list;
+
+	for (int i = 0; i < 6; i++)
+	{
+		list.push_back(i);
+	}
+
+	int sum = 0;
+
+	for (_iterator<int> it = list.begin(); it != list.end(); it++)
+	{
+		sum += it.value();
+		std::cout << it.value() << std::endl;
+	}
+
+	sum += list.end().value();
+	std::cout << list.back() << std::endl;
+
+	std::cout << "----------" << std::endl;
+	std::cout << "Sum: " << sum << std::endl;
+}
+
+void TestTwo()
+{
+	List<char> list;
+	list.push_back('d');
+	list.push_back('l');
+	list.push_back('r');
+	list.push_back('o');
+	list.push_back('w');
+	list.push_back(' ');
+	list.push_back('o');
+	list.push_back('l');
+	list.push_back('l');
+	list.push_back('e');
+	list.push_back('h');
+
+	List<char> reverse;
+
+	for (_iterator<char> it = list.begin(); it != list.end(); it++)
+	{
+		reverse.push_front(it.value());
+	}
+
+	reverse.push_front(list.back());
+
+	std::cout << "\n\nFirst List:\n";
+	for (auto it = list.begin(); it != list.end(); it++)
+	{
+		std::cout << it.value() << "_";
+	}
+	std::cout << list.back();
+
+	std::cout << "\n\nSecond List:\n";
+	for (auto it = reverse.begin(); it != reverse.end(); it++)
+	{
+		std::cout << it.value() << "_";
+	}
+	std::cout << reverse.back();
+}
+
+void TestThree()
+{
+	List<int> list;
+	list.push_back(-10);
+	list.push_back(0);
+	list.push_back(2);
+	list.push_back(9);
+	list.push_back(0);
+	list.push_back(-15);
+	list.push_back(-6);
+	list.push_back(-7);
+	list.push_back(0);
+	list.push_back(13);
+	list.push_back(8);
+	list.push_back(4);
+	list.push_back(0);
+	list.push_back(6);
+
+	List<int> negatives;
+	List<int> zeros;
+	List<int> positives;
+
+	for (auto it = list.begin(); it != list.end(); it++)
+	{
+		int tmp = it.value();
+		if (tmp < 0)
+		{
+			negatives.push_back(tmp);
+		}
+		else if (tmp == 0)
+		{
+			zeros.push_back(tmp);
+		}
+		else
+		{
+			positives.push_back(tmp);
+		}
+	}
+
+	int tmp = list.back();
+	if (tmp < 0)
+	{
+		negatives.push_back(tmp);
+	}
+	else if (tmp == 0)
+	{
+		zeros.push_back(tmp);
+	}
+	else
+	{
+		positives.push_back(tmp);
+	}
+
+	std::cout << "\n\nNegative List:\n_____________\n";
+	for (auto it = negatives.begin(); it != negatives.end(); it++)
+	{
+		std::cout << it.value() << "_";
+	}
+	std::cout << negatives.back();
+
+	std::cout << "\n\nZero List:\n_____________\n";
+	for (auto it = zeros.begin(); it != zeros.end(); it++)
+	{
+		std::cout << it.value() << "_";
+	}
+	std::cout << zeros.back();
+
+	std::cout << "\n\nPositive List:\n_____________\n";
+	for (auto it = positives.begin(); it != positives.end(); it++)
+	{
+		std::cout << it.value() << "_";
+	}
+	std::cout << positives.back();
+}
+
+
+void TestFour()
+{
+	List<int> list;
+	list.push_back(10);
+	list.push_back(2);
+	list.push_back(9);
+	list.push_back(15);
+	list.push_back(6);
+	list.push_back(7);
+	list.push_back(13);
+	list.push_back(8);
+
+	List<int> odds;
+	List<int> evens;
+
+	for (auto it = list.begin(); it != list.end(); it++)
+	{
+		int tmp = it.value();
+		if (tmp % 2 == 0)
+		{
+			evens.push_back(tmp);
+		}
+		else
+		{
+			odds.push_back(tmp);
+		}
+	}
+
+	int tmp = list.back();
+	if (tmp % 2 == 0)
+	{
+		evens.push_back(tmp);
+	}
+	else
+	{
+		odds.push_back(tmp);
+	}
+
+	std::cout << "\n\nOdds List:\n_____________\n";
+	for (auto it = odds.begin(); it != odds.end(); it++)
+	{
+		std::cout << it.value() << "_";
+	}
+	std::cout << odds.back();
+
+	std::cout << "\n\nEvens List:\n_____________\n";
+	for (auto it = evens.begin(); it != evens.end(); it++)
+	{
+		std::cout << it.value() << "_";
+	}
+	std::cout << evens.back();
+}
+
+void TestFive()
+{
+	List<int> list;
+	list.push_back(-1);
+	list.push_back(5);
+	list.push_back(17);
+	list.push_back(4);
+	list.push_back(9);
+	list.push_back(20);
+	list.push_back(-7);
+	list.push_back(8);
+	list.push_back(11);
+	list.push_back(0);
+	list.push_back(53);
+	list.push_back(14);
+
+	// TODO: sort.
+}
+
+int main()
+{
+	TestOne();
+
+	TestTwo();
+
+	TestThree();
+
+	TestFour();
+
+	TestFive();
+
+	system("PAUSE");
+	return 0;
+}
