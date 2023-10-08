be655cd1edc001d765ea183d53b9b79cca94b47d
Hidd3n-47 <122316898+Hidd3n-47@users.noreply.github.com>
2023-10-08 15:55:13 +0100

    Comments, printing abstraction, inserting into the list and assertions.
    Comments added to explain what the functions do/return.
    Added insert method to insert an element into the list at some position.
    Extra operators added for adding to iterator as well as [ ] operator for list.
    Assertions added for error checking.
    Macro added for printing for printing abstraction.

--- Assigmnment1/List.h
+++ Assigmnment1/List.h
@@ -1,37 +1,42 @@
 #pragma once
 
-struct _MemAllocation
+#define ASSERT(x) if(x) __debugbreak();
+
+// Struct to hold the memory that has been allocated and freed. Unnamed to have single instance.
+struct
 {
 	uint32_t _allocated		= 0; 
 	uint32_t _freed			= 0;
-};
+} MEM_ALLOCATION;
 
-static _MemAllocation MEM_ALLOCATION;
-
+// 'new' Keyword override.
 void* operator new(size_t size)
 {
 	MEM_ALLOCATION._allocated += size;
 	return malloc(size);
 }
 
+// 'delete' Keyword override.
 void operator delete(void* memory, size_t size)
 {
 	MEM_ALLOCATION._freed += size;
 	free(memory);
 }
 
-template<typename T>
+// Internal struct used to hold information for the list.
+template<class T>
 struct _List_Node
 {
 	_List_Node() = default;
 	_List_Node(T value) : _Value(value) { /* Empty. */ }
 
-	T				_Value	= NULL;
+	T				_Value	= { };
 	_List_Node<T>*	_Next	= nullptr;
 	_List_Node<T>*	_Prev	= nullptr;
 };
 
-template<typename T>
+// Iterator class for iterating through a list.
+template<class T>
 class _iterator
 {
 	template<typename T>
@@ -39,52 +44,97 @@ class _iterator
 public:
 	T value() const { return _Node->_Value; }
 
+	// [Post] Increment iterator to next iteration.
 	inline _iterator<T>& operator++(int)
 	{
-		// TODO (Christian): Add assert here if there is no next value.
+		ASSERT(!_Node->_Next);	/* Ensure that there is another value for the iterator to increment to.		*/	
+
 		this->_Node = _Node->_Next;
 
 		return *this;
 	}
 
+	// [Pre] Increment iterator to next iteration.
 	inline _iterator<T>& operator++()
 	{
-		// TODO (Christian): Add assert here if there is no next value.
+		ASSERT(!_Node->_Next);	/* Ensure that there is another value for the iterator to increment to.		*/
+
 		this->_Node = _Node->_Next;
 
 		return *this;
 	}
 
+	// Query if two iterators are equal.
 	inline bool operator== (const _iterator<T>& _rhs) const
 	{
 		return (this->_Node == _rhs._Node);
 	}
 
+	// Query if two iterators are not equal.
 	inline bool operator!= (const _iterator<T>& _rhs) const
 	{
 		return (this->_Node != _rhs._Node);
 	}
 
+	// Move iterator by '_num' counts to the right.
+	inline _iterator<T> operator+(unsigned int _num)
+	{
+		ASSERT(_num == 0); // Ensure that a valid number is added to the operator.
+
+		_iterator<T>* _it = this;
+		int _counter = 0;
+		while (_counter++ != _num)
+		{
+			ASSERT(!(_it->_Node->_Next));			/* Ensure the iterator is not list.end().				*/
+			ASSERT(!(_it->_Node->_Next->_Next));	/* Ensure that the next iterator is not list.end().		*/
+
+
+			_it->_Node = _it->_Node->_Next;
+		}
+
+		return *_it;
+	}
+
+	// Move the iterator by '_num' counts to the left. NOTE: only works for _num == 1.
+	inline _iterator<T> operator-(unsigned int _num)
+	{
+		ASSERT(_num != 1);		/* Check that it is only '_iterator - 1'. Not implemented for subtracting more values.	*/
+		ASSERT(!this->_Node);	/* Ensure that the Node exists, i.e. the list is non-empty.								*/
+
+		return _iterator<T>(this->_Node->_Prev);
+	}
 private:
 	_iterator(_List_Node<T>* _node) : _Node(_node) { /* Empty. */ }
 	_List_Node<T>* _Node;
 };
 
-template<typename T>
+// A double linked-list of any type T.
+template<class T>
 class List
 {
 public:
 	inline List<T>()	{ _End = new _List_Node<T>(); }
 	inline ~List<T>()	{ clear(); delete _End; }
 
+	// Create a list with multiple values inserted in.
+	template<class ...Args>
+	inline List<T>(Args&&... _args)
+	{
+		_End = new _List_Node<T>();
+		([&] { push_back(_args); } (), ...);
+	}
+
 	/// <<< ----- Inserting into the List.
-	template<typename ...Args>
+
+	// Add all the values passed in into the back of list. Note, inserted in the order they are passed in.
+	template<class ...Args>
 	inline void push_back(Args&&... _args)
 	{
 		([&] { push_back(_args); } (), ...);
 	}
 
-	inline void push_back(T _Value) // Refactored.
+	// Add a single value into the back of the list.
+	inline void push_back(T _Value)
 	{
 		++_MySize;
 
@@ -99,7 +149,8 @@ public:
 		_Last = _new;
 	}
 
-	inline void push_front(T _Value) // Refactored.
+	// Add a single value into the front of the list.
+	inline void push_front(T _Value)
 	{
 		++_MySize;
 
@@ -112,13 +163,24 @@ public:
 		_First = _new;
 	}
 
-	// TODO (Christian): Implement insert method: void insert(_iterator _Where, T* _Value);
+	// Insert a value at the given position of the iterator.
+	inline void insert(_iterator<T> _Position, T _Value)
+	{
+		_List_Node<T>* _new = new _List_Node<T>(_Value);
+		
+		_new->_Next = _Position._Node;
+		_new->_Prev = _Position._Node->_Prev;
+		_new->_Prev->_Next = _new;
+		_Position._Node->_Prev = _new;
+	}
 	/// ================================
 
 	/// <<< ----- Removing from the List.
+
+	// Remove one item from the back of the list.
 	inline void pop_back()
 	{
-		if (_MySize == 0) { return; } // TODO (Christian): Assert here due to the list being already empty.
+		ASSERT(_MySize == 0);	/* Ensure that you are not removing an element from an empty list.	*/
 
 		--_MySize;
 
@@ -135,9 +197,10 @@ public:
 		_Last->_Next = _End;
 	}
 
+	// Remove one item from the front of the list.
 	inline void pop_front()
 	{
-		if (_MySize == 0) { return; } // TODO (Christian): Assert here due to the list being already empty.
+		ASSERT(_MySize == 0);	/* Ensure that you are not removing an element from an empty list.	*/
 
 		--_MySize;
 
@@ -153,9 +216,10 @@ public:
 		_First->_Prev = nullptr;
 	}
 
+	// Clear the list to have no values.
 	inline void clear()
 	{
-		if (_MySize == 0) { return; } // TODO (Christian): Assert here due to the list being already empty.
+		ASSERT(_MySize == 0); 	/* Ensure that you are not clearing an empty list.	*/
 
 		_List_Node<T>* node = _First;
 
@@ -174,9 +238,11 @@ public:
 		_End->_Prev = nullptr;
 	}
 
+	// Swap the values of two items in the list. 
+	// Note: ONLY swaps the values of the items and not the memory addresses, therefore be careful for stale pointers when swapping.
 	inline void swap(_iterator<T> _val1, _iterator<T> _val2)
 	{
-		// TODO (Christian): Assert if the iterators are the same.
+		ASSERT(_val1 == _val2);		/* Ensures that you cannot swap an element with itself.		*/
 
 		T tmp = _val1.value();
 
@@ -186,16 +252,40 @@ public:
 	/// ================================
 
 	///<<< ----- Iterators.
-	inline _iterator<T> begin()		{ return _iterator<T>(_First); }
-	inline _iterator<T> end()		{ return _iterator<T>(_End); }
-	/// ================================
 
+	// Iterator pointing to the beginning of the list, i.e. the first item in the list.
+	inline _iterator<T> begin() const	{ return _iterator<T>(_First); }
+	// Iterator pointing to one after the last item on the list.
+	inline _iterator<T> end()	const	{ return _iterator<T>(_End); }
+
+	/// ================================
 
 	///<<< ----- Accessors.
-	inline T front() const			{ return _First->_Value; }
-	inline T back() const			{ return _Last->_Value; }
+
+	// Returns the first value of the list.
+	inline T front()		const	{ return _First->_Value; }
+	// Returns the last value of the list.
+	inline T back()			const	{ return _Last->_Value; }
+	// Returns the number of items in the list.
 	inline uint64_t size()	const	{ return _MySize; }
+
 	/// ================================
+
+	// Operator to acces the _index item in the list.
+	inline T& operator[](unsigned int _index)
+	{
+		ASSERT(_index >= _MySize);		/* Ensure that the index is not outside the size of the list.	*/
+
+		_List_Node<T>* _nodePtr = _First;
+
+		unsigned int _counter = 0;
+		while (_counter++ != _index)
+		{
+			_nodePtr = _nodePtr->_Next;
+		}
+
+		return _nodePtr->_Value;
+	}
 private:
 	_List_Node<T>* _First	= nullptr;
 	_List_Node<T>* _Last	= nullptr;
--- Assigmnment1/main.cpp
+++ Assigmnment1/main.cpp
@@ -2,191 +2,28 @@
 
 #include "List.h"
 
-#include <list>
-
-void TestOne()
+template<class T>
+void ConsolePrintList(const List<T>& list)
 {
-	std::cout << "\n|=============================================================|\n";
-	std::cout <<   "|                           TEST_ONE                          |\n";
-	std::cout <<   "|-------------------------------------------------------------|\n";
-
-	List<int> list;
-
-	for (int i = 0; i < 6; i++)
+	std::cout << "{ ";
+	for (auto it = list.begin(); it != list.end(); it++)
 	{
-		list.push_back(i);
+		std::cout << it.value();
+		if (it != list.end() - 1)
+		{
+			std::cout << ", ";
+		}
 	}
-
-	int sum = 0;
-
-	std::cout << "| List:\n| ";
-
-	for (_iterator<int> it = list.begin(); it != list.end(); it++)
-	{
-		sum += it.value();
-		std::cout << it.value() << ", ";
-	}
-
-	std::cout << std::endl;
-	std::cout << "| Sum: " << sum << std::endl;
-	std::cout << "|=============================================================|\n";
-
+	std::cout << " }" << std::endl;
 }
 
-void TestTwo()
+#define PRINT(x) \
+	std::cout << "| '" << #x << "' = "; \
+	ConsolePrintList(x)
+
+template<class T>
+void BubbleSortList(List<T>& list)
 {
-	std::cout << "\n|=============================================================|\n";
-	std::cout <<   "|                           TEST_TWO                          |\n";
-	std::cout <<   "|-------------------------------------------------------------|\n";
-
-	List<char> list;
-	list.push_back('d', 'l', 'r', 'o', 'w', ' ', 'o', 'l', 'l', 'e', 'h');
-
-	List<char> reverse;
-
-	for (_iterator<char> it = list.begin(); it != list.end(); it++)
-	{
-		reverse.push_front(it.value());
-	}
-
-	std::cout << "| First List:\n| ";
-	for (auto it = list.begin(); it != list.end(); it++)
-	{
-		std::cout << it.value() << ", ";
-	}
-
-	std::cout << "\n| Second List:\n| ";
-	for (auto it = reverse.begin(); it != reverse.end(); it++)
-	{
-		std::cout << it.value() << ", ";
-	}
-
-	std::cout << std::endl;
-	std::cout << "|=============================================================|\n";
-}
-
-void TestThree()
-{
-	std::cout << "\n|=============================================================|\n";
-	std::cout <<   "|                         TEST_THREE                          |\n";
-	std::cout <<   "|-------------------------------------------------------------|\n";
-
-	List<int> list;
-	list.push_back(-10, 0, 2, 9, 0, -15, -6, -7, 0, 13, 8, 4, 0, 6);
-
-	List<int> negatives;
-	List<int> zeros;
-	List<int> positives;
-
-	for (auto it = list.begin(); it != list.end(); it++)
-	{
-		int tmp = it.value();
-		if (tmp < 0)
-		{
-			negatives.push_back(tmp);
-		}
-		else if (tmp == 0)
-		{
-			zeros.push_back(tmp);
-		}
-		else
-		{
-			positives.push_back(tmp);
-		}
-	}
-
-	std::cout << "| Original List:\n| ";
-	for (auto it = list.begin(); it != list.end(); it++)
-	{
-		std::cout << it.value() << ", ";
-	}
-
-	std::cout << "\n|\n| Negative List:\n| ";
-	for (auto it = negatives.begin(); it != negatives.end(); it++)
-	{
-		std::cout << it.value() << ", ";
-	}
-
-	std::cout << "\n| Zero List:\n| ";
-	for (auto it = zeros.begin(); it != zeros.end(); it++)
-	{
-		std::cout << it.value() << ", ";
-	}
-
-	std::cout << "\n| Positive List:\n| ";
-	for (auto it = positives.begin(); it != positives.end(); it++)
-	{
-		std::cout << it.value() << ", ";
-	}
-
-	std::cout << std::endl;
-	std::cout << "|=============================================================|\n";
-}
-
-
-void TestFour()
-{
-	std::cout << "\n|=============================================================|\n";
-	std::cout <<   "|                          TEST_FOUR                          |\n";
-	std::cout <<   "|-------------------------------------------------------------|\n";
-
-
-	List<int> list;
-	list.push_back(10, 2, 9, 15, 6, 7, 13, 8);
-
-	List<int> odds;
-	List<int> evens;
-
-	for (auto it = list.begin(); it != list.end(); it++)
-	{
-		int tmp = it.value();
-		if (tmp % 2 == 0)
-		{
-			evens.push_back(tmp);
-		}
-		else
-		{
-			odds.push_back(tmp);
-		}
-	}
-
-	std::cout << "| Original List:\n| ";
-	for (auto it = list.begin(); it != list.end(); it++)
-	{
-		std::cout << it.value() << ", ";
-	}
-
-	std::cout << "\n|\n| Odds List:\n| ";
-	for (auto it = odds.begin(); it != odds.end(); it++)
-	{
-		std::cout << it.value() << ", ";
-	}
-
-	std::cout << "\n| Evens List:\n| ";
-	for (auto it = evens.begin(); it != evens.end(); it++)
-	{
-		std::cout << it.value() << ", ";
-	}
-
-	std::cout << std::endl;
-	std::cout << "|=============================================================|\n";
-}
-
-void TestFive()
-{
-	std::cout << "\n|=============================================================|\n";
-	std::cout <<   "|                          TEST_FIVE                          |\n";
-	std::cout <<   "|-------------------------------------------------------------|\n";
-
-	List<int> list;
-	list.push_back(-1, 5, 17, 4, 9, 20, -7, 8, 11, 0, 53, 14);
-
-	std::cout << "| Original List:\n| ";
-	for (auto it = list.begin(); it != list.end(); it++)
-	{
-		std::cout << it.value() << ", ";
-	}
-
 	for (int i = list.size(); i > 0; i--)
 	{
 		int counter = 0;
@@ -203,26 +40,180 @@ void TestFive()
 			previous = it;
 		}
 	}
+}
 
-	std::cout << "\n| Sorted List:\n| ";
-	for (auto it = list.begin(); it != list.end(); it++)
+void TestOne()
+{
+	std::cout << "\n|===================================================================|\n";
+	std::cout <<   "|                              TEST_ONE                             |\n";
+	std::cout <<   "|-------------------------------------------------------------------|\n";
+
+	List<int> list;
+
+	for (int i = 0; i < 6; i++)
 	{
-		std::cout << it.value() << ", ";
+		list.push_back(i);
 	}
 
+	int sum = 0;
+
+	// Print method re-written here due to question stating "_Inside_ TestOne(), create a loop..."
+	// Could use PRINT(list); instead to display the contents of the list, which will be used
+	// in the other test methods.
+	std::cout << "| list = { ";
+	for (_iterator<int> it = list.begin(); it != list.end(); it++)
+	{
+		std::cout << it.value();
+		if (it != list.end() - 1)
+		{
+			std::cout << ", ";
+		}
+		sum += it.value();
+	}
+	std::cout << " }" << std::endl;
+
+	std::cout << "| Sum: " << sum << std::endl;
+	std::cout << "|===================================================================|\n";
+
+}
+
+void TestTwo()
+{
+	std::cout << "\n|===================================================================|\n";
+	std::cout <<   "|                              TEST_TWO                             |\n";
+	std::cout <<   "|-------------------------------------------------------------------|\n";
+
+	List<char> original;
+	original.push_back('d', 'l', 'r', 'o', 'w', ' ', 'o', 'l', 'l', 'e', 'h');
+
+	List<char> reverse;
+
+	for (_iterator<char> it = original.begin(); it != original.end(); it++)
+	{
+		reverse.push_front(it.value());
+	}
+
+	PRINT(original);
+
+	PRINT(reverse);
+
+	std::cout << "|===================================================================|\n";
+}
+
+void TestThree()
+{
+	std::cout << "\n|===================================================================|\n";
+	std::cout <<   "|                            TEST_THREE                             |\n";
+	std::cout <<   "|-------------------------------------------------------------------|\n";
+
+	List<int> original;
+	original.push_back(-10, 0, 2, 9, 0, -15, -6, -7, 0, 13, 8, 4, 0, 6);
+
+	List<int> negatives;
+	List<int> zeros;
+	List<int> positives;
+
+	for (auto it = original.begin(); it != original.end(); it++)
+	{
+		int tmp = it.value();
+		if (tmp < 0)
+		{
+			negatives.push_back(tmp);
+		}
+		else if (tmp == 0)
+		{
+			zeros.push_back(tmp);
+		}
+		else
+		{
+			positives.push_back(tmp);
+		}
+	}
+
+	PRINT(original);
+
+	PRINT(negatives);
+
+	PRINT(zeros);
+
+	PRINT(positives);
+
 	std::cout << std::endl;
-	std::cout << "|=============================================================|\n\n";
+	std::cout << "|===================================================================|\n";
+}
+
+
+void TestFour()
+{
+	std::cout << "\n|===================================================================|\n";
+	std::cout <<   "|                             TEST_FOUR                             |\n";
+	std::cout <<   "|-------------------------------------------------------------------|\n";
+
+
+	List<int> original;
+	original.push_back(10, 2, 9, 15, 6, 7, 13, 8);
+
+	List<int> odds;
+	List<int> evens;
+
+	for (auto it = original.begin(); it != original.end(); it++)
+	{
+		int tmp = it.value();
+		if (tmp % 2 == 0)
+		{
+			evens.push_back(tmp);
+		}
+		else
+		{
+			odds.push_back(tmp);
+		}
+	}
+
+	PRINT(original);
+
+	PRINT(odds);
+
+	PRINT(evens);
+
+	std::cout << "|===================================================================|\n";
+}
+
+void TestFive()
+{
+	std::cout << "\n|===================================================================|\n";
+	std::cout <<   "|                             TEST_FIVE                             |\n";
+	std::cout <<   "|-------------------------------------------------------------------|\n";
+
+	List<int> list;
+	list.push_back(-1, 5, 17, 4, 9, 20, -7, 8, 11, 0, 53, 14);
+
+	std::cout << "| Original List:" << std::endl;
+	PRINT(list);
+
+	BubbleSortList(list);
+
+	std::cout << "| Sorted List:" << std::endl;
+	PRINT(list);
+
+	std::cout << "|===================================================================|\n\n";
 }
 
 void PrintMemory()
 {
-	std::cout << "|=============================================================|\n";
-	std::cout << "|                       MEM_ALLOCATION                        |\n";
-	std::cout << "|-------------------------------------------------------------|\n";
+	std::cout << "|===================================================================|\n";
+	std::cout << "|                          MEM_ALLOCATION                           |\n";
+	std::cout << "|-------------------------------------------------------------------|\n";
 	std::cout << "| Allocated: "	<< MEM_ALLOCATION._allocated	<< " bytes\n";
 	std::cout << "| Freed: "		<< MEM_ALLOCATION._freed		<< " bytes\n";
-	std::cout << "|=============================================================|\n";
+	std::cout << "|===================================================================|\n";
 }
+
+class foo
+{
+public:
+	foo() { }
+	int arr[10] = { };
+};
 
 int main()
 {
@@ -234,12 +225,24 @@ int main()
 	TestFour();
 	TestFive();
 
+	// Memory Test 1.
 	{
 		List<double> list;
 		list.push_back(2., 4., 6.);
 		list.clear();
 		list.push_back(1., 3., 5.);
 	}
+
+	// Memory Test 2.
+	{
+		List<foo> list;
+		for (int i = 0; i < 10; i++)
+		{
+			foo bar;
+			list.push_back(bar);
+		}
+	}
+
 	PrintMemory();
 
 	std::cout << std::endl;
